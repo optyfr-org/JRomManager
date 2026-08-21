52a581081c37c4dace112e4242778f70499d9ee9
optyfr <17027109+optyfr@users.noreply.github.com>
2026-08-21 17:50:22 +0200

    fix(thread-safety): synchronize BatchTrntChkSDRXMLResponse add/update/remove with settings lock

--- jrmserver/src/main/java/jrm/server/shared/datasources/BatchTrntChkSDRXMLResponse.java
+++ jrmserver/src/main/java/jrm/server/shared/datasources/BatchTrntChkSDRXMLResponse.java
@@ -56,18 +56,21 @@ public class BatchTrntChkSDRXMLResponse extends SDRXMLResponse {
     @Override
     protected void add(Operation operation) throws XMLStreamException {
         if (operation.hasData("src")) {
-            final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
-            needSave(sdrl, SettingsEnum.trntchk_sdr);
             final var sdr = new SrcDstResult(operation.getData("src"));
-            Optional<SrcDstResult> candidate = sdrl.stream().filter(s -> s.getSrc().equals(operation.getData("src"))).findAny();
-            if (!candidate.isPresent()) {
-                sdrl.add(sdr);
-                request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
-                request.getSession().getUser().getSettings().saveSettings();
-                writeResponseSingle(sdr);
-            } else {
-                failure("Entry already exists");
+            synchronized (request.getSession().getUser().getSettings()) {
+                final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
+                needSave(sdrl, SettingsEnum.trntchk_sdr);
+                Optional<SrcDstResult> candidate = sdrl.stream().filter(s -> s.getSrc().equals(operation.getData("src"))).findAny();
+                if (!candidate.isPresent()) {
+                    sdrl.add(sdr);
+                    request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
+                    request.getSession().getUser().getSettings().saveSettings();
+                } else {
+                    failure("Entry already exists");
+                    return;
+                }
             }
+            writeResponseSingle(sdr);
         } else {
             failure(SRC_IS_MISSING_IN_REQUEST);
         }
@@ -86,30 +89,33 @@ public class BatchTrntChkSDRXMLResponse extends SDRXMLResponse {
             failure(SRC_IS_MISSING_IN_REQUEST);
             return;
         }
-        final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
-        needSave(sdrl, SettingsEnum.trntchk_sdr);
-        final Optional<SrcDstResult> candidate = sdrl.stream().filter(sdr -> sdr.getId().equals(operation.getData("id"))).findFirst();
-        if (!candidate.isPresent()) {
-            failure("not in list");
-            return;
+        final AbstractSrcDstResult result;
+        synchronized (request.getSession().getUser().getSettings()) {
+            final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
+            needSave(sdrl, SettingsEnum.trntchk_sdr);
+            final Optional<SrcDstResult> candidate = sdrl.stream().filter(sdr -> sdr.getId().equals(operation.getData("id"))).findFirst();
+            if (!candidate.isPresent()) {
+                failure("not in list");
+                return;
+            }
+            if (!operation.hasData("src") && !operation.hasData("dst") && !operation.hasData(SELECTED)) {
+                failure("field to update is missing in request");
+                return;
+            }
+            result = candidate.get();
+            if (operation.hasData("src")) {
+                result.setSrc(operation.getData("src"));
+            }
+            if (operation.hasData("dst")) {
+                result.setDst(operation.getData("dst"));
+            }
+            if (operation.hasData(SELECTED)) {
+                result.setSelected(Boolean.parseBoolean(operation.getData(SELECTED)));
+            }
+            request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
+            request.getSession().getUser().getSettings().saveSettings();
         }
-        if (!operation.hasData("src") && !operation.hasData("dst") && !operation.hasData(SELECTED)) {
-            failure("field to update is missing in request");
-            return;
-        }
-        final AbstractSrcDstResult sdr = candidate.get();
-        if (operation.hasData("src")) {
-            sdr.setSrc(operation.getData("src"));
-        }
-        if (operation.hasData("dst")) {
-            sdr.setDst(operation.getData("dst"));
-        }
-        if (operation.hasData(SELECTED)) {
-            sdr.setSelected(Boolean.parseBoolean(operation.getData(SELECTED)));
-        }
-        request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
-        request.getSession().getUser().getSettings().saveSettings();
-        writeResponseSingle(sdr);
+        writeResponseSingle(result);
     }
 
     /**
@@ -122,18 +128,22 @@ public class BatchTrntChkSDRXMLResponse extends SDRXMLResponse {
     @Override
     protected void remove(Operation operation) throws XMLStreamException {
         if (operation.hasData("id")) {
-            final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
-            needSave(sdrl, SettingsEnum.trntchk_sdr);
-            final Optional<SrcDstResult> candidate = sdrl.stream().filter(sdr -> sdr.getId().equals(operation.getData("id"))).findFirst();
-            if (candidate.isPresent()) {
-                final AbstractSrcDstResult sdr = candidate.get();
-                sdrl.remove(sdr);
-                request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
-                request.getSession().getUser().getSettings().saveSettings();
-                writeResponseKey(sdr);
-            } else {
-                failure("not in the list");
+            final AbstractSrcDstResult result;
+            synchronized (request.getSession().getUser().getSettings()) {
+                final SDRList<SrcDstResult> sdrl = SrcDstResult.fromJSON(request.getSession().getUser().getSettings().getProperty(SettingsEnum.trntchk_sdr));
+                needSave(sdrl, SettingsEnum.trntchk_sdr);
+                final Optional<SrcDstResult> candidate = sdrl.stream().filter(sdr -> sdr.getId().equals(operation.getData("id"))).findFirst();
+                if (candidate.isPresent()) {
+                    result = candidate.get();
+                    sdrl.remove(result);
+                    request.getSession().getUser().getSettings().setProperty(SettingsEnum.trntchk_sdr, AbstractSrcDstResult.toJSON(sdrl));
+                    request.getSession().getUser().getSettings().saveSettings();
+                } else {
+                    failure("not in the list");
+                    return;
+                }
             }
+            writeResponseKey(result);
         } else {
             failure(SRC_IS_MISSING_IN_REQUEST);
         }
